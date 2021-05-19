bf8bffdc2c8181e3ebe659d6aadc0de4daa48955
hayoungzzzzz <hayoung474@gmail.com>
2021-05-19 16:35:47 +0900

    글쓰기 폼 그리드 수정, 글쓰기 토글 수정 , 보호소 데이터 서버 연동

--- front-end/src/pages/Shelter/ShelterAnimalListContainer.js
+++ front-end/src/pages/Shelter/ShelterAnimalListContainer.js
@@ -3,48 +3,30 @@ import ShelterAnimalListPresenter from "./ShelterAnimalListPresenter";
 import axios from "axios";
 function ShelterAnimalListContainer() {
   const [ShelterAnimalData, setShelterAnimalData] = useState([]);
-  const serviceKey ="g4fjxGQYBDsO7DJoSVH4qbE9pCV7knL71oKLyPbukZeY5tbq%2BY2GoDr6EqXF1DaQ7Zr%2F4mJvB6Lia9cf%2B1DbGQ%3D%3D";
-  const URL = `http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic?&pageNo=1&numOfRows=30&ServiceKey=${serviceKey}`
+  const URL = "http://192.168.234.178:5000/shelter/animal"
 
-  function fetchData(url) {
+  function fetchData(select) {
+    let tempURL=URL
+    if (select !== "전체"){
+      tempURL += ("/"+select)
+    } 
+    console.log(tempURL)
     axios
-      .get(url)
-      .then((res) => {
-        setShelterAnimalData(res.data.response.body.items.item);
-        console.log(res.data.response.body.items.item);
-      })
-      .catch((err) => {
-        console.log(err);
-        console.log("데이터 로드 실패");
-      });
+    .get(tempURL)
+    .then((res) => {
+      setShelterAnimalData(res.data.response.body.items.item);
+      console.log(res.data.response.body.items.item);
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log("데이터 로드 실패");
+    });
   }
   function AreaSelectChange(select) {
-    let areaCode = 0;
-    let tempURL = ""
-    if(select === "전체"){
-      areaCode = 0;
-      tempURL = URL;
-    }
-    else{
-      if (select === "서울특별시") {
-        areaCode = 6110000;
-      }
-      else if (select === "경상북도") {
-        areaCode = 6470000;
-      }
-      else if (select === "전라북도") {
-        areaCode = 6450000;
-      }
-      else if (select === "강원도") {
-        areaCode = 6420000;
-      }
-      tempURL = URL + `&upr_cd=${areaCode}`;
-    }
-    fetchData(tempURL);
-
+    fetchData(select)
   }
   useEffect(() => {
-    fetchData(URL);
+    fetchData("전체");
   }, []); // mounted 와 같은 효과
 
   return (
--- front-end/src/pages/WritePost/WritePostContainer.js
+++ front-end/src/pages/WritePost/WritePostContainer.js
@@ -1,10 +1,39 @@
-import React from 'react'
-import WritePostPresenter from './WritePostPresenter'
+import React, { useState } from "react";
+import WritePostPresenter from "./WritePostPresenter";
+import axios from "axios";
 function WritePostContainer() {
+  const [postType, setPostType] = useState("실종신고");
+  function selectPostType(select){
+    console.log(select)
+    setPostType(select)
+    console.log(postType)
+  }
+  function createPost() {
+    const formdata = new FormData();
+
+    formdata.append("title", "hello mehee");
+    var config = {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "multipart/form-data",
+      },
+    };
+    axios
+      .post("http://192.168.234.178:5000/test", formdata, config)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
   return (
     <>
-      <WritePostPresenter></WritePostPresenter>
+      <WritePostPresenter
+        createPost={createPost}
+        selectPostType={selectPostType}
+      ></WritePostPresenter>
     </>
   );
 }
