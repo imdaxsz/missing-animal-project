2dc496ef2957f95517ca520bb841da635651afcc
imdaxsz <s2809h29@gmail.com>
2021-05-19 15:46:52 +0900

    지역 코드 추가 및 지역별 보호소 동물 조회

--- back-end/server.py
+++ back-end/server.py
@@ -13,6 +13,10 @@ app.config['JSON_AS_ASCII'] = False
 def hello_world():
     return "hello"
 
+sido_code = {'6110000':'서울특별시', '6260000':'부산광역시', '6270000':'대구광역시', '6280000':'인천광역시', '6290000':'광주광역시', '5690000':'세종특별자치시',
+             '6300000':'대전광역시', '6310000':'울산광역시', '6410000':'경기도', '6420000':'강원도','6430000':'충청북도', '6440000':'충청남도', '6450000':'전라북도',
+             '6460000':'전라남도', '6470000':'경상북도', '6480000':'경상남도', '6500000':'제주특별자치도'}
+
 @app.route('/shelter/animal', methods=['GET'])
 def shelter_animal():
     if request.method == 'GET':
@@ -31,11 +35,18 @@ def shelter_animal():
         dict['header'] = 1
         return dict
 
+def find_sido_code(sido):
+    for key, value in sido_code.items():
+        if sido == value:
+            return key
+    return "There is no such Key"
+
 @app.route('/shelter/animal/<sido>')
 def shelter(sido):
-    return 'abc : ' + sido
+    code = find_sido_code(sido)
+    #return code
 
-    url = 'http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic?pageNo=1&numOfRows=10'
+    url = 'http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic?upr_cd=' + code + '&pageNo=1&numOfRows=10'
     queryParams = '&' + urlencode({quote_plus(
         'ServiceKey'): 'g4fjxGQYBDsO7DJoSVH4qbE9pCV7knL71oKLyPbukZeY5tbq%2BY2GoDr6EqXF1DaQ7Zr%2F4mJvB6Lia9cf%2B1DbGQ%3D%3D'})
 
