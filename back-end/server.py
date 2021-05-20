47cff56852b2efef978a6b850abf2fd8b6a779c3
imdaxsz <s2809h29@gmail.com>
2021-05-20 15:11:19 +0900

    api로부터 시군구 정보 받아와 저장

--- back-end/server.py
+++ back-end/server.py
@@ -9,15 +9,60 @@ import xmltodict
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
+sido_code = {'6110000':{'6110000':'서울특별시'}, '6260000':{'6260000':'부산광역시'}, '6270000':{'6270000':'대구광역시'}, '6280000':{'6280000':'인천광역시'},
+             '6290000':{'6290000':'광주광역시'}, '5690000':{'5690000':'세종특별자치시'}, '6300000':{'6300000':'대전광역시'}, '6310000':{'6310000':'울산광역시'},
+             '6410000':{'6410000':'경기도'}, '6420000':{'6420000':'강원도'}, '6430000':{'6430000':'충청북도'}, '6440000':{'6440000':'충청남도'},
+             '6450000':{'6450000':'전라북도'}, '6460000':{'6460000':'전라남도'}, '6470000':{'6470000':'경상북도'}, '6480000':{'6480000':'경상남도'},
+                                                      '6500000':{'6500000':'제주특별자치도'}}
+
+def find_sido_code(sido):
+    keylist = list(sido_code)
+    for k in keylist:
+        if sido == sido_code[k][k]:
+            return k
+    return "There is no such Key"
+
+def get_sigungu():
+    sidokey = list(sido_code)
+    sido_list = []
+    for k in range(len(sidokey)):
+        if k == 5:
+            continue
+        url = 'http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/sigungu?upr_cd='+ sidokey[k]
+        queryParams = '&' + urlencode({quote_plus(
+            'ServiceKey'): 'g4fjxGQYBDsO7DJoSVH4qbE9pCV7knL71oKLyPbukZeY5tbq%2BY2GoDr6EqXF1DaQ7Zr%2F4mJvB6Lia9cf%2B1DbGQ%3D%3D'})
+
+        request = urllib.request.Request(url + unquote(queryParams))
+        print('Your Request:\n' + url + queryParams)
+        request.get_method = lambda: 'GET'
+        response_body = urlopen(request).read()
+
+        result = xmltodict.parse(response_body)
+        dict = json.loads(json.dumps(result))
+        dict['header'] = 1
+        temp = dict['response']['body']['items']
+        tmplist = temp['item'] #type = list
+        sido_list += tmplist
+
+        for i in range(len(sido_list)):
+            for k in range(len(sidokey)):
+                if sido_list[i]['uprCd'] == sidokey[k]:
+                    key = sidokey[k]
+                    orgcd = sido_list[i]['orgCd']
+                    sido_code[key][orgcd] = sido_list[i]['orgdownNm']
+    return sido_code
+
+get_sigungu()
+print("결과:")
+print(sido_code)
+
+
 @app.route('/')
 def hello_world():
     return "hello"
 
-sido_code = {'6110000':'서울특별시', '6260000':'부산광역시', '6270000':'대구광역시', '6280000':'인천광역시', '6290000':'광주광역시', '5690000':'세종특별자치시',
-             '6300000':'대전광역시', '6310000':'울산광역시', '6410000':'경기도', '6420000':'강원도','6430000':'충청북도', '6440000':'충청남도', '6450000':'전라북도',
-             '6460000':'전라남도', '6470000':'경상북도', '6480000':'경상남도', '6500000':'제주특별자치도'}
 
-@app.route('/shelter/animal', methods=['GET'])
+@app.route('/shelter/animal', methods=['GET']) #유기동물 정보조회
 def shelter_animal():
     if request.method == 'GET':
         # 보호소 유기동물 정보
@@ -35,17 +80,11 @@ def shelter_animal():
         dict['header'] = 1
         return dict
 
-def find_sido_code(sido):
-    for key, value in sido_code.items():
-        if sido == value:
-            return key
-    return "There is no such Key"
 
-@app.route('/shelter/animal/<sido>')
+@app.route('/shelter/animal/<sido>') #시도별 유기동물 정보 조회
 def shelter(sido):
     code = find_sido_code(sido)
-    #return code
-
+    print("code:", code)
     url = 'http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic?upr_cd=' + code + '&pageNo=1&numOfRows=10'
     queryParams = '&' + urlencode({quote_plus(
         'ServiceKey'): 'g4fjxGQYBDsO7DJoSVH4qbE9pCV7knL71oKLyPbukZeY5tbq%2BY2GoDr6EqXF1DaQ7Zr%2F4mJvB6Lia9cf%2B1DbGQ%3D%3D'})
@@ -60,6 +99,7 @@ def shelter(sido):
     dict['header'] = 1
     return dict
 
+
 @app.route('/test', methods=['POST'])
 def test_post():
    if request.method == 'POST':
@@ -69,3 +109,4 @@ def test_post():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
